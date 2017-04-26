8e439d1e4f83a87697c8ac934962531f66e33fe5
blueswen <blueswenlove@gamil.com>
2017-04-26 15:57:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd042606/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yvtc.yvd042606;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyHelper helper = new MyHelper(MainActivity.this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.close();
+    }
+
+
+}
